afea289f7d0ac30d08a12a4a75190eef8b8efa3c
paulineforslind <paufor595@ihmeducation.se>
2024-09-19 14:41:10 +0200

    Created A1 Project

---
+++ A1/js/app.js
@@ -0,0 +1,28 @@
+// VARIABLES
+  // Buttons
+let button0 = document.getElementById("button0");
+
+ // Texts
+let playerRollText = document.getElementById("playerRollText");
+
+ // Data
+let playerRoll = 0;
+
+
+// PROCESS
+button0.addEventListener("click", function() {
+  getRandomNumberOneToSixForPlayer();
+  showPlayerRollResult();
+});
+
+
+// CONTROLLERS
+function getRandomNumberOneToSixForPlayer() {
+  playerRoll = Math.floor(Math.random() * 6) + 1;
+}
+
+
+// VIEWS
+function showPlayerRollResult() {
+  playerRollText.innerHTML = playerRoll;
+}
